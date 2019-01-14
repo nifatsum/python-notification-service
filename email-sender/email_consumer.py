f8ed6a625823e4e721426452266f2ff1b23c8b7b
alibek.mustafin <alibek.mustafin@avalab.io>
2019-01-14 17:37:27 +0500

    email_consumer - typo fix.

--- email-sender/email_consumer.py
+++ email-sender/email_consumer.py
@@ -19,7 +19,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
-email_sender = EmailSender();
+email_sender = EmailSender()
 
 
 def callback(ch, method, properties, body):
